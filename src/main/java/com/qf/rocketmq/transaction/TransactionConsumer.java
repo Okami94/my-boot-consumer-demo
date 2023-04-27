d80401fdc4a6246f239bc6320f6574e5f5bd1a25
wang <exsgundam@outlook.com>
2023-04-27 13:13:01 +0800

    事务消息

---
+++ src/main/java/com/qf/rocketmq/transaction/TransactionConsumer.java
@@ -0,0 +1,35 @@
+package com.qf.rocketmq.transaction;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+//事务消息
+public class TransactionConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+        //1.创建消费对象
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("my-consumer-group-1");
+        //2.指明namesever的地址
+        consumer.setNamesrvAddr("192.168.159.130:9876;192.168.159.131:9876;192.168.159.129:9876");
+        //3.订阅主题：topic 和过滤消息用的tag表达式
+        consumer.subscribe("TopicTest", "*");
+        //4.创建一个监听器，当broker把消息推过来调用
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                for (MessageExt msg : list) {
+                    System.out.println("收到的消息：" + msg);
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        //5.启动消费者
+        consumer.start();
+        System.out.println("消费者已启动");
+    }
+}
